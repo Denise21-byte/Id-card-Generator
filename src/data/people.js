6e56b527cb075910d1d39558e025014b27ccf47c
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-03-05 14:00:30 +0200

    adding the functionality on cards in people.js

---
+++ src/data/people.js
@@ -0,0 +1,25 @@
+const people = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    role: "Software Engineer",
+    department: "Engineering",
+    avatar: "https://i.pravatar.cc/150?img=1"
+  },
+  {
+    id: 2,
+    name: "John Smith",
+    role: "UI/UX Designer",
+    department: "Design",
+    avatar: "https://i.pravatar.cc/150?img=2"
+  },
+  {
+    id: 3,
+    name: "Grace Lee",
+    role: "Data Analyst",
+    department: "Analytics",
+    avatar: "https://i.pravatar.cc/150?img=3"
+  }
+];
+
+export default people;
